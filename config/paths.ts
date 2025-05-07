39c3ced785637164aa55218575202635d46d38e2
Авцынов Сергей <avtsynov.s@yandex.ru>
2025-05-07 23:05:25 +0300

    change path

--- config/paths.ts
+++ config/paths.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 export const paths = {
   src: path.resolve(__dirname, "../src"),
-  build: path.resolve(__dirname, "../dist"),
+  build: path.resolve(__dirname, "../build"),
   public: path.resolve(__dirname, "../public"),
   fonts: path.join("fonts", "[name].[contenthash][ext]"),
   icons: path.join("icons", "[name].[contenthash][ext]"),
